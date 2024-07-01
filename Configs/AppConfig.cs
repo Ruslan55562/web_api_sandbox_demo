c9461f91be6a4a9a7485ee245c39897be170a16e
Ruslan <ruslan.kl@proj.com>
2024-07-01 10:27:14 +0300

    added appConfig & DbHook, upd appsettings

---
+++ Configs/AppConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SpecFlowProject
+{
+    public static class AppConfig
+    {
+        private static readonly Lazy<IConfiguration> _configuration = new(() => new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build());
+
+        public static string? GetConnectionString()
+        {
+            return _configuration.Value.GetConnectionString("HsqldbConnection");
+        }
+
+        public static string? GetUser()
+        {
+            return _configuration.Value["HsqldbSettings:User"];
+        }
+
+        public static string? GetPassword()
+        {
+            return _configuration.Value["HsqldbSettings:Password"];
+        }
+    }
+}
---
+++ Hooks/DBHook.cs
@@ -0,0 +1,52 @@
+﻿using java.sql;
+using NUnit.Framework;
+using SpecFlowProject;
+using System.Data;
+
+namespace web_api_sandbox_demo_UI.Hooks
+{
+    public static class DBHook
+    {
+        private static Connection? _connection;
+
+        public static void OpenConnection()
+        {
+            try
+            {
+                string? connectionString = AppConfig.GetConnectionString();
+                string? user = AppConfig.GetUser();
+                string? password = AppConfig.GetPassword();
+
+                DriverManager.registerDriver(new org.hsqldb.jdbcDriver());
+                _connection = DriverManager.getConnection(connectionString, user, password);
+                _connection.setAutoCommit(true);
+            }
+            catch (SQLException ex)
+            {
+                Console.WriteLine("Exception was thrown when connecting to DB " + ex.Message);
+                throw;
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            try
+            {
+                if (_connection != null && !_connection.isClosed())
+                {
+                    _connection.close();
+                }
+            }
+            catch (SQLException ex)
+            {
+                Console.WriteLine("There is an exception when trying to close connection " + ex.Message);
+                throw;
+            }
+        }
+
+        public static bool IsConnectionOpen()
+        {
+                return _connection != null && !_connection.isClosed();
+        }
+    }
+}
